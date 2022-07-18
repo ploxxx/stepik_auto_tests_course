6701f50eba818506b355a6d307608fc8ad9ba5c3
Vlad Zharkov <zharkov.kill@yandex.by>
2022-07-18 15:16:59 +0300

    fixture

--- part3/test_abs_project.py
+++ part3/test_abs_project.py
@@ -4,8 +4,8 @@ class TestAbs(unittest.TestCase) :
     def test_abs1(self):
          self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
         
-    def test_abs2(self):
-        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+    def abs2(self):
+        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
         
 if __name__ == "__main__":
     unittest.main()
