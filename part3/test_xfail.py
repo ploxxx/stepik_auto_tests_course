99e36683367b453b06600870055e32c7792be9ae
Vlad Zharkov <zharkov.kill@yandex.by>
2022-07-18 16:33:09 +0300

    create new lesson file

---
+++ part3/test_xfail.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+@pytest.mark.xfail(strict=True)
+def test_succeed():
+    assert True
+
+
+@pytest.mark.xfail
+def test_not_succeed():
+    assert False
+
+
+@pytest.mark.skip
+def test_skipped():
+    assert False
